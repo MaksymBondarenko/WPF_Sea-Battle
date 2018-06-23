f60a2dfa9d342373119cdd792a9fa43a710a5281
MaksymBondarenko <maksymbondarenko1980@gmail.com>
2018-06-23 23:38:05 +0300

    23.06.2018

--- WpfApp1/WpfApp1/Startpage.xaml.cs
+++ WpfApp1/WpfApp1/Startpage.xaml.cs
@@ -19,10 +19,70 @@ namespace WpfApp1
     /// </summary>
     public partial class Window1 : Window
     {
+        int Kol_One=4, Kol_Two=3, Kol_Three=2, Kol_Four=1, n=0;
+        Random RND = new Random();
+        List<Button> Buttons;
         public Window1()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loadded;
+            Buttons = new List<Button>();
+            foreach (var item in grid.Children)
+            {
+
+                if (item is Grid)
+                {
+                    foreach (var child1 in (item as Grid).Children)
+                    {
+                        if (item is Grid)
+                        {
+                            foreach (var child in (item as Grid).Children)
+                            {
+
+                                if (child is Button)
+                                {
+                                    if ((child as Button).Tag.ToString() != "0")
+                                    {
+                                        Buttons.Add(child as Button);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
+        private void MainWindow_Loadded(object sender, RoutedEventArgs e)
+        {
+            int i=0;
+            while (i!=10)
+            {
+                int Shot = RND.Next(0, 100);
+                if (Buttons[Shot].Tag.ToString() == "false")
+                {
+                    Buttons[Shot].Template = Resources["ButtonWound"] as ControlTemplate;
+                }
+                else { Buttons[Shot].Template = Resources["ButtonShot"] as ControlTemplate; }
+                i++;
+            }
+
+
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+           
+            ////if (dialog.ShowDialog() == true)
+            ////{
+            ////    // Пользователь разрешил действие. Продолжить1
+            ////}
+            ////else
+            ////{
+            ////    // Пользователь отменил действие.
+            ////}
+            this.Close();
+            
+        }
     }
 }
